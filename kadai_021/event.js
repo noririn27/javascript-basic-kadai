778e60e9c7425c889298bb60cacc6709ebce65c2
nori <dr.norirish@gmail.com>
2025-04-08 10:23:09 +0900

    021初回

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+    const text = document.getElementById('text');
+    text.textContent = 'ボタンをクリックしました';
+  }, 2000);
+});
